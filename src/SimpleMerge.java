f18553a9a66243454b572d9e1f07be6d3877c060
ItsMeEthan <43207586+ItsMeEthan@users.noreply.github.com>
2019-02-09 21:54:19 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -7,27 +7,27 @@
     public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
         int[] m = new int[arr1.length + arr2.length];
-        int x = 0;
-        int y = 0;
+        int x1 = 0;
+        int x2 = 0;
         for (int mm = 0; mm < m.length; mm++) {
-            if (x >= arr1.length || y >= arr2.length) {
+            if (x1 >= arr1.length || x2 >= arr2.length) {
                 if (x >= arr1.length) {
-                    m[mm] = arr2[y];
+                    m[mm] = arr2[x2];
                     y++;
                 } else {
-                    m[mm] = arr1[x];
+                    m[mm] = arr1[x1];
                     x++;
                 }
             } else {
-                if (arr1[x] < arr2[y]) {
-                    if (x < arr1.length) {
-                        m[mm] = arr1[x];
+                if (arr1[x1] < arr2[x2]) {
+                    if (x1 < arr1.length) {
+                        m[mm] = arr1[x1];
                         x++;
                     }
                 } else {
                     if (y < arr2.length) {
-                        m[mm] = arr2[y];
-                        y++;
+                        m[mm] = arr2[x2];
+                        x2++;
                     }
                 }
 
